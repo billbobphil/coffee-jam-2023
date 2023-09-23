7b17eebdffe1cc081c9f5ee9133acae95cf73ad9
billbobphil <37590467+billbobphil@users.noreply.github.com>
2023-09-23 10:48:31 -0700

    Added station art and table art

--- scripts/Station.gd
+++ scripts/Station.gd
@@ -1,5 +1,10 @@
 extends StaticBody2D
 
+var coffeeStationSprite = preload("res://art/coffee-station-64x64.png");
+var espressoStationSprite = preload("res://art/espresso-station-64x64.png");
+var sugarAndCreamStationSprite = preload("res://art/sugarAndCreamStation-64x64.png");
+var steamerStation = preload("res://art/steamer-station-64x64.png");
+
 var keyPromptSprite;
 var stationCanBeInteractedWith = false;
 @export var stationName: String = "";
@@ -11,6 +16,14 @@ func _ready():
 	keyPromptSprite = get_node("Key Prompt Sprite");
 	var player = get_tree().get_first_node_in_group("Player")
 	player.interaction_triggered.connect(_on_player_interaction_triggered)
+	if(productType == Product.productTypes.coffee):
+		get_node("Station Sprite").texture = coffeeStationSprite;
+	if(productType == Product.productTypes.espresso):
+		get_node("Station Sprite").texture = espressoStationSprite;
+	if(productType == Product.productTypes.steamedMilk):
+		get_node("Station Sprite").texture = steamerStation;
+	if(productType == Product.productTypes.sugarAndCream):
+		get_node("Station Sprite").texture = sugarAndCreamStationSprite;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
