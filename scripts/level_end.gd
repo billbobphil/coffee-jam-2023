eb3466f6ecbe8ccaab8eb5cdf80f805dc7e5c826
billbobphil <37590467+billbobphil@users.noreply.github.com>
2023-09-26 18:04:12 -0700

    Added button sound effects

--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -9,8 +9,18 @@ func showDialog(customersServed, totalRevenue):
 
 
 func _on_play_again_button_pressed():
+	$ButtonClickEffect.play()
 	get_tree().reload_current_scene();
 
 
 func _on_menu_button_pressed():
+	$ButtonClickEffect.play()
 	get_tree().change_scene_to_file("res://scenes/mainMenu.tscn");
+
+
+func _on_play_again_button_mouse_entered():
+	$ButtonHoverEffect.play();
+
+
+func _on_menu_button_mouse_entered():
+	$ButtonHoverEffect.play();
--- scripts/mainMenu.gd
+++ scripts/mainMenu.gd
@@ -4,4 +4,9 @@ func _ready():
 	get_tree().paused = false;
 
 func _on_play_button_pressed():
+	$ButtonClickSoundEffect.play();
 	get_tree().change_scene_to_file("res://scenes/rootScene.tscn");
+
+func _on_play_button_mouse_entered():
+	$ButtonHoverSoundEffect.play();
+	
