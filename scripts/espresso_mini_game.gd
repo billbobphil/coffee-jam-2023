8736b4f64370156ffadeaa2b89c4770a408e76c6
billbobphil <37590467+billbobphil@users.noreply.github.com>
2023-09-27 17:55:42 -0700

    Added art to the espresso minigame - need to add sound effects!

--- scripts/espresso_mini_game.gd
+++ scripts/espresso_mini_game.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var successSprite : Texture;
 var numberOfSuccesses : int = 0;
 
+@export var espressoPuckHalfSprite : Texture;
+@export var espressoPuckDoneSprite : Texture;
+
 signal espresso_mini_game_completed;
 
 func _on_area_2d_body_entered(body):
@@ -17,7 +20,9 @@ func updateSuccessSprites():
 	print("Updating");
 	if(numberOfSuccesses == 1):
 		$SuccessIndicator_One.texture = successSprite;
+		get_node("EspressoPuck/Sprite2D").texture = espressoPuckHalfSprite;
 	elif(numberOfSuccesses == 2):
 		$SuccessIndicator_Two.texture = successSprite;
+		get_node("EspressoPuck/Sprite2D").texture = espressoPuckDoneSprite;
 	elif(numberOfSuccesses == 3):
 		$SuccessIndicator_Three.texture = successSprite;
